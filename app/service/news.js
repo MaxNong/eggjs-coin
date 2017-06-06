767f46da4fafd74e9b693d033afd62b25f1d0635
baoqiang <002207@zjzxlh.cn>
2017-06-06 18:32:05 +0800

    添加文件

---
+++ app/service/news.js
@@ -0,0 +1,33 @@
+module.exports = app => {
+  class NewsService extends app.Service {
+    * list(page = 1) {
+      // read config
+      const { serverUrl, pageSize } = this.app.config.news;
+      // use build-in http client to GET hacker-news api
+      const { data: idList } = yield this.ctx.curl(`${serverUrl}/topstories.json`, {
+        data: {
+          orderBy: '"$key"',
+          startAt: `"${pageSize * (page - 1)}"`,
+          endAt: `"${pageSize * page - 1}"`,
+        },
+        dataType: 'json',
+      });
+      // parallel GET detail, see `yield {}` from co
+      const newsList = yield Object.keys(idList).map(key => {
+        const url = `${serverUrl}/item/${idList[key]}.json`;
+        return this.ctx.curl(url, { dataType: 'json' });
+      });
+      return newsList.map(res => res.data);
+    }
+  }
+  return NewsService;
+};
+module.exports = app => {
+  class User extends app.Service {
+    * find(id) {
+      const user = yield this.ctx.db.query(`select name from user where id = "1"`);
+      return user;
+    }
+  }
+  return User;
+};
--- config/config.default.js
+++ config/config.default.js
@@ -1,1 +1,30 @@
-exports.keys = 212121
+exports.keys = 'UserName=BaoQiang'
+exports.view = {
+  defaultViewEngine: 'nunjucks',
+  mapping: {
+    '.tpl': 'nunjucks',
+  },
+};
+exports.news = {
+  pageSize: 5,
+  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
+exports.mysql = {
+  // 单数据库信息配置
+  client: {
+    // host
+    host: 'localhost',
+    // 端口号
+    port: '3306',
+    // 用户名
+    user: 'root',
+    // 密码
+    password: '',
+    // 数据库名
+    database: 'xfd',
+  },
+  // 是否加载到 app 上，默认开启
+  app: true,
+  // 是否加载到 agent 上，默认关闭
+  agent: false,
+};
